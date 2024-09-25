0a7cdb0a6c64072b837bed246e80e57573a156ef
Alexander Oviedo Fadul <45949794+bladealex9848@users.noreply.github.com>
2024-09-25 13:36:53 -0500

    Commit inicial del proyecto, incluido el archivo .gitignore, LICENSE, README.md, app.py, requirements.txt y la carpeta assets con las imágenes logo.jpg, logo..jpg y logo...jpg

---
+++ README.md
@@ -0,0 +1,50 @@
+![Logo de TrasladoJudicialBot](https://github.com/bladealex9848/TrasladoJudicialBot/blob/main/assets/logo.jpg)
+
+# TrasladoJudicialBot ⚖️
+
+## Descripción
+TrasladoJudicialBot es un agente de IA especializado en el sistema judicial colombiano, con un enfoque particular en los traslados de los servidores judiciales.
+
+## Funcionalidades
+* Define qué es un traslado en el contexto judicial colombiano.
+* Explica los diferentes tipos de traslados que existen para los servidores judiciales y los requisitos para cada uno.
+* Brinda información sobre los procedimientos y plazos para solicitar un traslado.
+* Asesora sobre los recursos legales disponibles en caso de que una solicitud de traslado sea denegada.
+* Proporciona información actualizada sobre la normativa aplicable a los traslados.
+
+## Limitaciones
+* No puede brindar asesoramiento legal personalizado.
+* Su conocimiento se basa en la legislación colombiana vigente hasta el 17 de junio de 2022.
+* Siempre es recomendable consultar con un abogado o asesor legal para obtener orientación específica sobre un caso particular.
+
+## Instalación
+
+1. Asegúrate de tener Python 3.8 o superior instalado en tu máquina.
+2. Clona este repositorio: `git clone https://github.com/bladealex9848/TrasladoJudicialBot.git`
+3. Navega al directorio del proyecto: `cd TrasladoJudicialBot`
+4. Instala las dependencias: `pip install -r requirements.txt`
+5. Crea un archivo `.streamlit/secrets.toml` y añade tu ASSISTANT_ID de OpenAI:
+   ```
+   ASSISTANT_ID = "tu-assistant-id-aqui"
+   ```
+6. Configura tu clave API de OpenAI como variable de entorno o en el archivo `secrets.toml`.
+
+## Uso
+
+1. Ejecuta la aplicación: `streamlit run app.py`
+2. Abre tu navegador y ve a `http://localhost:8501`
+3. Comienza a interactuar con el Asistente Virtual escribiendo tus preguntas o solicitudes.
+
+## Contribuciones
+
+Las contribuciones son bienvenidas. Por favor, haz un fork del repositorio, crea una nueva rama para tus cambios, y envía un pull request.
+
+## Licencia
+
+Este proyecto está bajo la licencia MIT. Consulta el archivo `LICENSE` para más detalles.
+
+## Autor
+
+Creado por Alexander Oviedo Fadul
+
+[GitHub](https://github.com/bladealex9848) | [Website](https://alexanderoviedofadul.dev) | [LinkedIn](https://www.linkedin.com/in/alexander-oviedo-fadul/) | [Instagram](https://www.instagram.com/alexander.oviedo.fadul) | [Twitter](https://twitter.com/alexanderofadul) | [Facebook](https://www.facebook.com/alexanderof/) | [WhatsApp](https://api.whatsapp.com/send?phone=573015930519&text=Hola%20!Quiero%20conversar%20contigo!%20)
---
+++ app.py
@@ -0,0 +1,157 @@
+# Importación de bibliotecas necesarias
+import os
+import openai
+import streamlit as st
+import time
+
+import streamlit as st
+import openai
+
+# Configuración de la página
+st.set_page_config(
+    page_title="TrasladoJudicialBot",
+    page_icon="⚖️",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Get Help': 'https://marduk.pro/',
+        'Report a bug': None,
+        'About': "TrasladoJudicialBot: Tu asistente especializado en traslados de servidores judiciales en Colombia. Obtén información sobre requisitos, procedimientos y plazos."
+    }
+)
+
+# Función para verificar si el archivo secrets.toml existe
+def secrets_file_exists():
+    secrets_path = os.path.join('.streamlit', 'secrets.toml')
+    return os.path.isfile(secrets_path)
+
+# Intentar obtener el ID del asistente de OpenAI desde st.secrets si el archivo secrets.toml existe
+if secrets_file_exists():
+    try:
+        ASSISTANT_ID = st.secrets['ASSISTANT_ID']
+    except KeyError:
+        ASSISTANT_ID = None
+else:
+    ASSISTANT_ID = None
+
+# Si no está disponible, pedir al usuario que lo introduzca
+if not ASSISTANT_ID:
+    ASSISTANT_ID = st.sidebar.text_input('Introduce el ID del asistente de OpenAI', type='password')
+
+# Si aún no se proporciona el ID, mostrar un error y detener la ejecución
+if not ASSISTANT_ID:
+    st.sidebar.error("Por favor, proporciona el ID del asistente de OpenAI.")
+    st.stop()
+
+assistant_id = ASSISTANT_ID
+
+# Inicialización del cliente de OpenAI
+client = openai
+
+st.title("Bienvenido a TrasladoJudicialBot ⚖️")
+
+st.write("""
+        [![ver código fuente](https://img.shields.io/badge/Repositorio%20GitHub-gris?logo=github)](https://github.com/bladealex9848/TrasladoJudicialBot)
+        ![Visitantes](https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Ftrasladojudicialbot.streamlit.app&label=Visitantes&labelColor=%235d5d5d&countColor=%231e7ebf&style=flat)
+        """)
+
+st.markdown("""
+### ⚖️ ¡Hola! Soy TrasladoJudicialBot, tu asistente en traslados de servidores judiciales
+
+Estoy aquí para brindarte información y apoyo sobre los traslados en la Rama Judicial colombiana
+
+#### ¿Qué puedo hacer por ti hoy? 🤔
+
+* Explicarte qué es un traslado en el contexto judicial colombiano
+* Describir los diferentes tipos de traslados y sus requisitos
+* Guiarte en el procedimiento para solicitar un traslado
+* Informarte sobre los plazos y recursos legales disponibles
+* Brindar información actualizada sobre la normativa aplicable
+
+**¡No dudes en consultarme cualquier inquietud sobre traslados de servidores judiciales!**
+
+*Recuerda: Proporciono información basada en mi conocimiento actual y en la normativa vigente hasta el 17 de junio de 2022. Para asesoría legal o técnica específica, consulta a un experto.*
+""")
+
+# Inicialización de variables de estado de sesión
+st.session_state.start_chat = True
+if "thread_id" not in st.session_state:
+    st.session_state.thread_id = None
+
+# Cargar la clave API de OpenAI
+API_KEY = os.environ.get('OPENAI_API_KEY') or st.secrets.get('OPENAI_API_KEY')
+if not API_KEY:
+    API_KEY = st.sidebar.text_input('Introduce tu clave API de OpenAI', type='password')
+
+if not API_KEY:
+    st.sidebar.error("Por favor, proporciona una clave API para continuar.")
+    st.stop()
+
+openai.api_key = API_KEY
+
+def process_message_with_citations(message):
+    """Extraiga y devuelva solo el texto del mensaje del asistente."""
+    if hasattr(message, 'content') and len(message.content) > 0:
+        message_content = message.content[0]
+        if hasattr(message_content, 'text'):
+            nested_text = message_content.text
+            if hasattr(nested_text, 'value'):
+                return nested_text.value
+    return 'No se pudo procesar el mensaje'
+
+# Crear un hilo de chat inmediatamente después de cargar la clave API
+if not st.session_state.thread_id:
+    thread = client.beta.threads.create()
+    st.session_state.thread_id = thread.id
+    st.write("ID del hilo: ", thread.id)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("¿Cómo puedo ayudarte hoy?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("usuario"):
+        st.markdown(prompt)
+
+    # Enviar mensaje del usuario
+    client.beta.threads.messages.create(
+        thread_id=st.session_state.thread_id,
+        role="user",
+        content=prompt
+    )
+
+    # Crear una ejecución para el hilo de chat
+    run = client.beta.threads.runs.create(
+        thread_id=st.session_state.thread_id,
+        assistant_id=assistant_id
+    )
+
+    while run.status != 'completed':
+        time.sleep(1)
+        run = client.beta.threads.runs.retrieve(
+            thread_id=st.session_state.thread_id,
+            run_id=run.id
+        )
+
+    # Recuperar mensajes agregados por el asistente
+    messages = client.beta.threads.messages.list(
+    thread_id=st.session_state.thread_id
+    )
+
+    # Procesar y mostrar mensajes del asistente
+    for message in messages:
+        if message.run_id == run.id and message.role == "assistant":
+            full_response = process_message_with_citations(message)
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
+            with st.chat_message("assistant"):
+                st.markdown(full_response)
+                
+# Footer
+st.sidebar.markdown('---')
+st.sidebar.subheader('Creado por:')
+st.sidebar.markdown('Alexander Oviedo Fadul')
+st.sidebar.markdown("[GitHub](https://github.com/bladealex9848) | [Website](https://alexanderoviedofadul.dev/) | [LinkedIn](https://www.linkedin.com/in/alexander-oviedo-fadul/) | [Instagram](https://www.instagram.com/alexander.oviedo.fadul) | [Twitter](https://twitter.com/alexanderofadul) | [Facebook](https://www.facebook.com/alexanderof/) | [WhatsApp](https://api.whatsapp.com/send?phone=573015930519&text=Hola%20!Quiero%20conversar%20contigo!%20)")
